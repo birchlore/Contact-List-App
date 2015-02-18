4ee5e71e0e064f4336984833560f46a22cbf1b74
birchlore <jacksondcunningham@gmail.com>
2015-02-17 19:40:14 -0800

    config database

--- config/database.rb
+++ config/database.rb
@@ -10,16 +10,27 @@ configure do
   # }
 
 
-db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/contact_app')
-                                    # postgres://localhost/[YOUR_DATABASE_NAME]
-set :database, {
-     adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-     host:     db.host,
-     # username: db.user,
-     # password: db.password,
-     database: db.path[1..-1],
-     encoding: 'utf8'
-}
+# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/contact_app')
+#                                     # postgres://localhost/[YOUR_DATABASE_NAME]
+# set :database, {
+#      adapter:  db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#      host:     db.host,
+#      username: db.user,
+#      password: db.password,
+#      database: db.path[1..-1],
+#      encoding: 'utf8'
+# }
+
+db = URI.parse('postgres://postgres:12345@localhost/contact_app')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
