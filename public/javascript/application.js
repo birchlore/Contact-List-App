7b979369d0b719e5cc899c70ed86d48fd1e67243
birchlore <jacksondcunningham@gmail.com>
2015-02-19 01:45:05 +0000

    fix jquery html var

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -38,17 +38,16 @@ var inlineEdit = function() {
 
 
   var successCallback = function(data) {
+      $('#contact-list').empty();
 
-          $('#contact-list').empty();
-          data.forEach(function(contact) {
+      data.forEach(function(contact) {
+          html =  '<div class="contact" id="'+contact.id+'"><div class="contact-header text-center"><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="name" class="contact-name">'+contact.name+'</a> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><span class="glyphicon glyphicon-minus" aria-hidden="true"></span><span class="glyphicon glyphicon-remove delete-contact" aria-hidden="true"></span> </div><div class="contact-body"><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="email" class="contact-email">'+contact.email+'</a><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="number" class="contact-number">'+contact.number+'</a></div></div>';
+          $('#contact-list').append(html);
+      });
 
-            html =  '<div class="col-sm-4 contact" id="'+contact.id+'"><div class="contact-header"><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="name" class="contact-name">'+contact.name+'</a> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span><span class="glyphicon glyphicon-minus" aria-hidden="true"></span><span class="glyphicon glyphicon-remove delete-contact" aria-hidden="true"></span></div><div class="contact-body"><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="email" class="contact-email">'+contact.email+'</a><a href ="/post" data-type="text" data-pk="'+contact.id+'" data-url="/post" data-name="number" class="contact-number">'+contact.number+'</a></div></div>'
-            
-            $('#contact-list').append(html);
-          });
-          $('.form-inline').find('input').val('');
-          basicListeners();
-        }
+      $('.form-inline').find('input').val('');
+      basicListeners();
+    }
 
   var removeContact = function() {
     $('.glyphicon-remove').click(function(){
